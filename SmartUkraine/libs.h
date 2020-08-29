6ce903ffcd43c9c71e9a6ff17211cf135ef81dcb
elik <elik@example.com>
2020-08-29 16:30:53 +0300

    Загрузка файлів

---
+++ SmartUkraine/libs.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <iostream>
+#include <Windows.h>
+#include <conio.h>
+#include <string>
+#include <map>
+#include <fstream>
+#include <iterator>
+#include <ctime>
+#include <vector>
+#include <filesystem>
+
+
+using namespace std;
+
+void SetPos(int x, int y);
+
+void SetCol(int a);
+
+int Autefication();
+
+void AntiCursor();
+
+void Reception();
+
+string GetPath();
